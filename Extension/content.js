64c2794fc7145b36cfa06dda2aa5397b0369537f
VincentS <vs38190@gmail.com>
2024-02-13 23:43:56 +0100

    Added compatibility to numbers from 1 to 3999999

--- Extension/content.js
+++ Extension/content.js
@@ -1,15 +1,47 @@
+const roman_dict = {
+    1: "I",
+    4: "IV",
+    5: "V",
+    9: "IX",
+    10: "X",
+    40: "XL",
+    50: "L",
+    90: "XC",
+    100: "C",
+    400: "CD",
+    500: "D",
+    900: "CM",
+    1000: "M",
+    4000: "<span style='text-decoration: overline;'>IV</span>",
+    5000: "<span style='text-decoration: overline;'>V</span>",
+    9000: "<span style='text-decoration: overline;'>IX</span>",
+    10000: "<span style='text-decoration: overline;'>X</span>",
+    40000: "<span style='text-decoration: overline;'>XL</span>",
+    50000: "<span style='text-decoration: overline;'>L</span>",
+    90000: "<span style='text-decoration: overline;'>XC</span>",
+    100000: "<span style='text-decoration: overline;'>C</span>",
+    400000: "<span style='text-decoration: overline;'>CD</span>",
+    500000: "<span style='text-decoration: overline;'>D</span>",
+    900000: "<span style='text-decoration: overline;'>DM</span>",
+    1000000: "<span style='text-decoration: overline;'>M</span>",
+}
+
+const roman_keys = Object.keys(roman_dict);
+const roman_values = Object.values(roman_dict);
+const roman_length = roman_keys.length;
+
+
 // Convert roman numbers to arabic numbers
 function convertToRoman(num) {
-    if (num < 1 || num > 3999) {
+    if (num < 1 || num > 3999999) {
         return num;
     }
-    const romans = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"];
-    const values = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
+
     let romanNumeral = '';
-    for (let i = 12; i >= 0; i--) {
-        while (num >= values[i]) {
-        num -= values[i];
-        romanNumeral += romans[i];
+    for (let i = roman_length-1; i >= 0; i--) {
+        while (num >= roman_keys[i]) {
+            num -= roman_keys[i];
+            romanNumeral += roman_values[i];
         }
     }
     return romanNumeral;
@@ -30,21 +62,31 @@ function memoize(func) {
 }
 
 function checkStringAndReplace(string) {
+    // Return false if the string does not contain a number
+    // else return the new string with the numbers replaced by their roman equivalent
     const regex = /\d+/g;
-    const span_style = 'font-family: cursive;';
+    const span_style = 'font-family: Impact;';
     const begin_span = '<span style="' + span_style + '">';
     const end_span = '</span>';
     const replaceFunction = function(match) {
         return begin_span + memoize(convertToRoman)(parseInt(match)) + end_span;
     };
-    return string.replace(regex, replaceFunction);
+
+    let new_string = string.replace(regex, replaceFunction);
+    if (new_string !== string) {
+        return new_string;
+    } else {
+        return false;
+    }
 }
 
 function convertTextToNode(textNode, parentNode) {
     // Convert the text node to a span node
     const span = document.createElement('span');
     const new_html_content = checkStringAndReplace(textNode.textContent);
-
+    if (new_html_content === false) {
+        return; // If the text node does not contain a number, do not replace it
+    }
     span.innerHTML = new_html_content;
     parentNode.replaceChild(span, textNode);
 }
@@ -65,14 +107,13 @@ function replaceNumbersRec(node, parentNode, end_execution_time) {
     }
 }
 
-// Remplacer tous les nombres sur la page par des chiffres romains
-function replaceNumbersWithRoman(begin_time = Date.now()) {
-    const max_execution_time = 1000; // Maximum execution time in milliseconds
+// Function to replace all numbers in the text content of the page
+function replaceNumbersWithRoman(max_execution_time = 2500) { // Maximum execution time in milliseconds
+    const begin_time = Date.now(); // Start time in milliseconds
     const end_execution_time = begin_time + max_execution_time; // Maximum execution time in milliseconds
     console.log('The page is being processed');
     replaceNumbersRec(document.body, null, end_execution_time);
 }
 
-console.log('The page has been loaded');
-replaceNumbersWithRoman(begin_time = Date.now());
-  
+// Execute when the page is loaded
+replaceNumbersWithRoman();
